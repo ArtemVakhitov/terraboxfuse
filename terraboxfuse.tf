8bb7719d63ac2bbf1960c408a2a8244187f48452
Artem Vakhitov <artem.vakhitov@gmail.com>
2023-10-30 02:23:43 +0600

    Try self in local-exec to avoid cycle

--- terraboxfuse.tf
+++ terraboxfuse.tf
@@ -87,7 +87,7 @@ resource "yandex_compute_instance" "terra-prod" {
     command = <<EOF
 scp -3 -P 22 -i ~/.ssh/devops-eng-yandex-kp.pem -o StrictHostKeyChecking=no
 ubuntu@${yandex_compute_instance.terra-build.network_interface.0.nat_ip_address}:/tmp/boxfuse-sample-java-war-hello/target/hello-1.0.war
-ubuntu@${yandex_compute_instance.terra-prod.network_interface.0.nat_ip_address}:/tmp/
+ubuntu@${self.network_interface.0.nat_ip_address}:/tmp/
 EOF
   }
 
