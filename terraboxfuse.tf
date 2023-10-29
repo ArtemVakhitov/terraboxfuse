69c7189ce93a9e6dc631ed358dbcad746e281338
Artem Vakhitov <artem.vakhitov@gmail.com>
2023-10-29 20:59:26 +0600

    Add baseline instance creation code

--- terraboxfuse.tf
+++ terraboxfuse.tf
@@ -10,3 +10,29 @@ terraform {
 provider "yandex" {
   zone = "ru-central1-b"
 }
+
+resource "yandex_compute_instance" "terra" {
+
+  zone = "ru-central1-b"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8q5m87s3v0hmp06i5c"
+    }
+  }
+
+  network_interface {
+    subnet_id = "enpsmm7t50q5bg0tsak4"
+  }
+
+  metadata {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+
+}
